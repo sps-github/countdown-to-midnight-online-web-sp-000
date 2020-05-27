917739c3b5f3841d696f0e01ac67c8b392535373
Sarah Peters <sarahpieters15@gmail.com>
2020-05-27 09:20:54 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,7 +1,7 @@
 #write your code here
 seconds_countdown = 10
 
-def countdown(seconds)
+def countdown(seconds_countdown)
   while seconds_countdown > 0
     puts "#{seconds_countdown} SECOND(S)!"
     seconds_countdown -= 1
