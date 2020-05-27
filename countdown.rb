a94d0ba91c7db5a1488e782c968f00bd8fa1d989
Sarah Peters <sarahpieters15@gmail.com>
2020-05-27 09:40:48 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
 #write your code here
+def countdown(seconds_countdown)
+  seconds_countdown = 10
+  while seconds_countdown > 0
+    puts "#{seconds_countdown} SECOND(S)!"
+    seconds_countdown -= 1
+    sleep(1)
+  end
+  return "HAPPY NEW YEAR!"
+end
 
-def countdown
+def countdown_with_sleep(seconds_countdown)
+  while seconds_countdown > 0
+    seconds_countdown -= 1
+    sleep(1)
+  end
 end
