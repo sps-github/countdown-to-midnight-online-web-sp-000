e522c1e8bec6225efbc055be39813637128125d6
Sarah Peters <sarahpieters15@gmail.com>
2020-05-27 09:20:12 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,10 @@
 #write your code here
+seconds_countdown = 10
 
-def countdown
+def countdown(seconds)
+  while seconds_countdown > 0
+    puts "#{seconds_countdown} SECOND(S)!"
+    seconds_countdown -= 1
+    sleep 1.second
+  end
 end
